0b228fe17606f971715057db48166ae06e18d797
Mehak <mehakmit@gmail.com>
2024-09-26 22:24:42 +0100

    Adding sign in page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
-from . import views
+from .views import signup
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    path('signup/', signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm
-from django.contrib.auth import login
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def signup(request):
     if request.method == 'POST':
-        form = SignupForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('login')
     else:
-        form = SignupForm()
+        form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
